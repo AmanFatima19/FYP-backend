b5776b54a88f2ce4323066ff632723660c69e4b6
AmanFatima19 <amanfatima713@gmail.com>
2025-08-25 01:59:18 +0500

    testing

--- testFiltering.js
+++ testFiltering.js
@@ -4,7 +4,7 @@ import Order from './models/order.js';
 
 async function testFiltering() {
   try {
-    await mongoose.connect('mongodb+srv://aman:aman1234@cluster0.ybfp8kw.mongodb.net/PAK-CARRY', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
